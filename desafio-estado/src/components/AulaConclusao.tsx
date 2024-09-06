3f239827e4b01f778202ac03a174799901de98ee
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2024-09-06 10:40:46 -0300

    responsividade

--- desafio-estado/src/components/AulaConclusao.tsx
+++ desafio-estado/src/components/AulaConclusao.tsx
@@ -9,7 +9,7 @@ export default function AulaConclusao(props: AulaConclusaoProps) {
     return (
         <button className={`
             flex items-center justify-center
-            w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 rounded-full border border-zinc-400
+            w-6 h-6 sm:w-4 sm:h-4 md:w-6 md:h-6 rounded-full border border-zinc-400
             ${props.concluida && 'bg-green-500'}
         `} onClick={props.onClick}>
             {props.concluida && <IconCheck size={18} stroke={3} />}
