dab70666553d958f19976114f21efefbd6b5303a
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2024-09-06 11:24:45 -0300

    feature scrollbar

--- desafio-estado/src/components/Player.tsx
+++ desafio-estado/src/components/Player.tsx
@@ -9,10 +9,10 @@ export default function Player() {
     return (
         <div className="
             flex flex-col justify-center items-center
-            w-full sm:w-11/12 h-60 sm:h-80 md:h-96 bg-zinc-700 rounded-lg p-3
+            sm:w-11/12 sm:h-80 md:h-3/5 bg-zinc-700 rounded-lg p-3
         ">
             <span className="text-lg sm:text-2xl md:text-3xl font-black">{aulaAtual.titulo}</span>
-            <div className="aspect-video w-full h-full p-2 sm:p-5 overflow-hidden">
+            <div className="aspect-video  h-full p-2 sm:p-5 overflow-hidden">
                 <iframe
                     width="100%"
                     height="100%"
